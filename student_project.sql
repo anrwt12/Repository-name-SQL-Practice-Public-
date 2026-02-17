d8be94d4e9cc50ca95011aa27995d00df9ab9d6f
Anjali Rawat <rawatanjali0513@gmail.com>
2026-02-17 20:32:40 +0530

    Add files via upload

---
+++ student_project.sql
@@ -0,0 +1,29 @@
+create database anjali_da;
+use anjali_da;
+-- create a table 
+create table student(
+rollno int primary key,
+name varchar(50),
+gender varchar(10),
+age int,
+city varchar(50)
+);
+
+ -- insert data
+ insert into student (rollno,name,gender,age,city) values
+ (101,'anjali', 'F',21,'nainital'),
+ (102,'rohan','M',24,'haldwani'),
+ (103,'risha','F',22,'rudrapur'),
+ (104,'rahul','M',25,'rampur');
+ 
+ -- select to display the data 
+ select* from student; -- * means everything
+ select gender from student;
+ select age from student;
+ 
+ -- want to see only top two rows
+ select*from student limit 2;
+ 
+ -- select last two rows
+ select*from student limit 2 offset 2;
+ select * from student limit 2 offset 1;
